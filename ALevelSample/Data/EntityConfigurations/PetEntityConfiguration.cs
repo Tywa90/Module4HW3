8e245a36ce66b6e5f65985c4e7eef1a90cdd3122
Serhii Amelin <amserega90@gmail.com>
2023-06-13 11:26:20 +0300

    fix FK errors

--- ALevelSample/Data/EntityConfigurations/PetEntityConfiguration.cs
+++ ALevelSample/Data/EntityConfigurations/PetEntityConfiguration.cs
@@ -16,5 +16,20 @@ public class PetEntityConfiguration : IEntityTypeConfiguration<PetEntity>
         builder.Property(p => p.LocationId).IsRequired();
         builder.Property(p => p.ImageUrl).HasMaxLength(255);
         builder.Property(p => p.Description).IsRequired();
+
+        builder.HasOne(h => h.Breed)
+            .WithMany(w => w.Pet)
+            .HasForeignKey(h => h.BreedID)
+            .OnDelete(DeleteBehavior.Cascade);
+
+        builder.HasOne(h => h.Category)
+           .WithMany(w => w.Pet)
+           .HasForeignKey(h => h.CategoryId)
+           .OnDelete(DeleteBehavior.Cascade);
+
+        builder.HasOne(h => h.Location)
+            .WithMany(w => w.Pet)
+            .HasForeignKey(h => h.LocationId)
+            .OnDelete(DeleteBehavior.Cascade);
     }
 }
